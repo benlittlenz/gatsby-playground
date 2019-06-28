2fc8b49158b052c3bbf203407e937a2ccc1315da
nzbenji <benlittle53@gmail.com>
2019-06-28 18:31:45 +1200

    data fetching slug page queries

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import Layout from '../components/layout';
 
-export default () => (
+const postTemplate = ({ data: post }) => (
     <Layout>
-        <h2>created blog post</h2>
+        <div>
+            <h1>{post.markdownRemark.frontmatter.title}</h1>
+            <div dangerouslySetInnerHTML={{
+              __html: post.markdownRemark.html  
+            }}/>
+        </div>
     </Layout>
 )
+
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: {
+            slug: { eq: $slug }
+        }){
+            html 
+            frontmatter {
+                title
+            }
+        }
+    }
+`
+
+export default postTemplate
