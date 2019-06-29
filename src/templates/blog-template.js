b9a2c032718d483c94f2f98025a8fb3857f6a523
nzbenji <benlittle53@gmail.com>
2019-06-29 13:54:01 +1200

    next/prev page pagination

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,51 +1,65 @@
 import React from 'react';
-import { StaticQuery, graphql, Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 
-const getMarkdownPosts = graphql`
-{
-  allMarkdownRemark {
-      totalCount
-         edges {
-            node {
-                fields {
-                    slug
-                }
-                id
-                frontmatter {
-                title
-                date
-                }
-                excerpt
-            }
-        }
-    }
-}
-`
+export default ({ data, pageContext}) => {
+    const { currentPage, isFirstPage, isLastPage } = pageContext;
+    const nextPage = `/blog/${String(currentPage + 1)}`
+    const prevPage = currentPage -1 === 1 ? '/blog' : `/blog${String(currentPage - 1)}`
 
-export default () => (
+    return (
     <Layout>
         <div>
             <h1
                 style={{display: 'inlineBlock', borderBottom: '1px solid'}}
             >Gatsby Blog</h1>
-            <StaticQuery 
-                query={getMarkdownPosts}
-                render={data => (
-                    <>
-                        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-                        {data.allMarkdownRemark.edges.map(({ node }) => (
-                            <div key={node.id}>
-                              <h3><Link to={`/posts/${node.fields.slug}`}>{node.frontmatter.title}</Link>
-                                    <span style={{color: '#bbb'}}> - {node.frontmatter.date}</span>
-                                </h3>
-                                <p>{node.excerpt}</p>
-                            </div>
-                        ))}
-                    </> 
-                )}
-            />
+                <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+                {data.allMarkdownRemark.edges.map(({ node }) => (
+                    <div key={node.id}>
+                    <h3><Link to={`/posts/${node.fields.slug}`}>{node.frontmatter.title}</Link>
+                            <span style={{color: '#bbb'}}> - {node.frontmatter.date}</span>
+                        </h3>
+                        <p>{node.excerpt}</p>
+                    </div>
+                ))}
+                <div>
+                    {!isFirstPage && (
+                        <Link to={prevPage} rel="prev">
+                            Prev Page
+                        </Link>
+                    )}
+                    {!isLastPage && (
+                        <Link to={nextPage} rel='next'>
+                            Next Page
+                        </Link>
+                    )}
+                </div>
         </div>
     </Layout>
-)
+    )
+}
+
+export const query = graphql`
+    query($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+        skip: $skip
+        limit: $limit
+    ) {
+        totalCount
+            edges {
+                node {
+                    fields {
+                        slug
+                    }
+                    id
+                    frontmatter {
+                    title
+                    date
+                    }
+                    excerpt
+                }
+            }
+        }
+    }
+`
